be9148e53f773a51ec4bd079e2b76f85df0a4ef9
Benjamin Smith <bsmith@cabbage.org.uk>
2022-10-28 12:40:29 +0100

    Tweak Song to a frozen dataclass

--- tests/song_test.py
+++ tests/song_test.py
@@ -1,4 +1,5 @@
 import unittest
+from dataclasses import FrozenInstanceError
 from src.song import Song
 
 class TestSong(unittest.TestCase):
@@ -10,3 +11,11 @@ class TestSong(unittest.TestCase):
 
     def test_song_has_artist(self):
         self.assertEqual("The Beatles", self.song.artist)
+
+    def test_song_is_frozen(self):
+        def try_changing_title():
+            self.song.title = "Eleanor Rigby"
+        def try_changing_artist():
+            self.song.artist = "Paul McCartney"
+        self.assertRaises(FrozenInstanceError, try_changing_title)
+        self.assertRaises(FrozenInstanceError, try_changing_artist)
