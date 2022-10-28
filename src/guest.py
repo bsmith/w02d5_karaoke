98b02fb154a5e01b24353e70cd0ae7131e93889c
Benjamin Smith <bsmith@cabbage.org.uk>
2022-10-28 14:10:33 +0100

    Implement Guest methods

--- src/guest.py
+++ src/guest.py
@@ -3,3 +3,12 @@ class Guest:
         self.name = name
         self.wallet = wallet
         self.paid_fee = False
+    
+    def can_afford_fee(self, fee_amount):
+        return fee_amount <= self.wallet
+    
+    def pay_fee(self, fee_amount):
+        if self.can_afford_fee(fee_amount):
+            self.wallet -= fee_amount
+            self.paid_fee = True
+        assert self.wallet > 0
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -14,23 +14,19 @@ class TestGuest(unittest.TestCase):
     def test_guest_has_not_paid_fee_yet(self):
         self.assertFalse(self.guest.paid_fee)
 
-    @unittest.skip("Fees and affordability not implemented yet")
     def test_guest_can_afford_fee__True(self):
         self.assertTrue(self.guest.can_afford_fee(25))
 
-    @unittest.skip("Fees and affordability not implemented yet")
     def test_guest_can_afford_fee__False(self):
         self.assertFalse(self.guest.can_afford_fee(100))
 
-    @unittest.skip("Fees and affordability not implemented yet")
     def test_guest_pay_fee__affordable(self):
-        self.pay_fee(25)
+        self.guest.pay_fee(25)
         self.assertEqual(self.guest.wallet, 25)
-        self.assertTrue(self.guest_paid_fee)
+        self.assertTrue(self.guest.paid_fee)
 
-    @unittest.skip("Fees and affordability not implemented yet")
     def test_guest_pay_fee__unaffordable(self):
-        self.pay_fee(100)
+        self.guest.pay_fee(100)
         self.assertEqual(self.guest.wallet, 50)
-        self.assertFalse(self.guest_paid_fee)
+        self.assertFalse(self.guest.paid_fee)
 
